49835a2936dfccee868fc8dd2d2a7d8bdf0f4528
Philip Tonaczew <tonaczew@gmail.com>
2023-01-13 12:32:57 +0100

    fist commit

---
+++ foodhunter/src/main/java/com/maremare/foodhunter/FoodHunterApplication.java
@@ -0,0 +1,16 @@
+package com.maremare.foodhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHunterApplication.class, args);
+
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+
+}
