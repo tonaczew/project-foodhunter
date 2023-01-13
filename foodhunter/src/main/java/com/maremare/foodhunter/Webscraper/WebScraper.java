a2cff5cb052fb304b7ccd348f60e79f6afc76374
Philip Tonaczew <tonaczew@gmail.com>
2023-01-13 17:23:16 +0100

    typing random htmlUnit lines for test

--- foodhunter/src/main/java/com/maremare/foodhunter/Webscraper/WebScraper.java
+++ foodhunter/src/main/java/com/maremare/foodhunter/Webscraper/WebScraper.java
@@ -1,7 +1,7 @@
 package com.maremare.foodhunter.Webscraper;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
 
 import java.io.IOException;
 
@@ -13,10 +13,18 @@ public class WebScraper {
         try(final WebClient webClient = new WebClient()){
             webClient.getOptions().setCssEnabled(false);
             webClient.getOptions().setJavaScriptEnabled(false);
-            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-            webClient.getOptions().setThrowExceptionOnScriptError(false);
-            webClient.getOptions().setPrintContentOnFailingStatusCode(false);
-            final HtmlPage page = webClient.getPage("https://www.koket.se/spanska-blamusslor-med-chorizo");
+            String url = "https://handlaprivatkund.ica.se/stores/1003418/";
+            final HtmlPage page = webClient.getPage(url);
+            HtmlForm form = (HtmlForm) page.getByXPath("//form").get(0);
+            HtmlInput displayNameField = form.getInputByName("q");
+            HtmlButton submitButton = (HtmlButton) form.getByXPath("//button").get(0);
+            displayNameField.type("kvarg");
+            HtmlPage resultsPage = submitButton.click();
+
+            HtmlHeading1 h1 = (HtmlHeading1) resultsPage.getByXPath("//main/div/div/ul/li/h1").get(0);
+
+            System.out.println(h1.getTextContent());
+
 
             return page.getTitleText();
         } catch (IOException e) {
