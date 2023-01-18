7fd8471a63f0ae3b30eb56769361b44dfde8bc3c
Philip Tonaczew <tonaczew@gmail.com>
2023-01-18 13:15:45 +0100

    refactoring

---
+++ foodhunter/src/main/java/com/maremare/foodhunter/Article.java
@@ -0,0 +1,20 @@
+package com.maremare.foodhunter;
+
+public class Article {
+
+    private final String productName;
+    private final String price;
+
+    public Article(String productName, String price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
--- foodhunter/src/main/java/com/maremare/foodhunter/Webscraper/IcaScraper.java
+++ foodhunter/src/main/java/com/maremare/foodhunter/Webscraper/IcaScraper.java
@@ -2,42 +2,74 @@ package com.maremare.foodhunter.Webscraper;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+import com.maremare.foodhunter.Article;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class IcaScraper {
 
+    final String url = "https://handlaprivatkund.ica.se/stores/1003418/";
+    final List<Article> articles = new ArrayList<>();
+    List<String> shoppingList = new ArrayList<>();
 
-    public String icaWebScrapePage(){
-        final String url = "https://handlaprivatkund.ica.se/stores/1003418/";
+
+    public String icaWebScrapePage() {
         String content = "";
 
-        try(final WebClient webClient = new WebClient()){
+        //shoppingList = createShoppingList();
+        shoppingList.add("kvarg");
+        shoppingList.add("tandkräm");
+        scrapeContent(shoppingList);
+
+        return content;
+    }
+
+    private List<String> createShoppingList() {
+        List<String> temp = new ArrayList<>();
+        try (Scanner scan = new Scanner(System.in)) {
+            String userInput;
+
+            do {
+                userInput = scan.nextLine().toLowerCase().trim();
+                temp.add(userInput);
+            } while (userInput.length() != 0);
+        }
+        return temp;
+    }
+
+    private void scrapeContent(List<String> shoppingList) {
+        try (final WebClient webClient = new WebClient()) {
             webClient.getOptions().setJavaScriptEnabled(false);
             webClient.getOptions().setCssEnabled(false);
+
             final HtmlPage page = webClient.getPage(url);
-            HtmlForm searchForm = (HtmlForm) page.getByXPath("//form").get(0);
-            HtmlInput inputTextField = searchForm.getInputByName("q");
-            HtmlButton searchButton = (HtmlButton) searchForm.getByXPath("//button").get(0);
-            inputTextField.type("kvarg naturell lindahls 500g");
-            HtmlPage resultPge = searchButton.click();
+            for (String product : shoppingList) {
+                final HtmlForm searchForm = (HtmlForm) page.getByXPath("//form").get(0);
+                final HtmlInput inputTextField = searchForm.getInputByName("q");
+                final HtmlButton searchButton = (HtmlButton) searchForm.getByXPath("//button").get(0);
+                inputTextField.type(product);
 
-            HtmlAnchor productContent = (HtmlAnchor) resultPge.getByXPath("//a[starts-with(@data-test,'fop-product-link')]").get(0);
-            HtmlStrong productPrice = (HtmlStrong) resultPge.getByXPath("//strong[starts-with(@data-test,'fop-price')]").get(0);
+                HtmlPage resultPge = searchButton.click();
+                HtmlAnchor productContent = (HtmlAnchor) resultPge.getByXPath("//a[starts-with(@data-test,'fop-product-link')]").get(0);
+                HtmlStrong productPrice = (HtmlStrong) resultPge.getByXPath("//strong[starts-with(@data-test,'fop-price')]").get(0);
 
-            String product = productContent.getTextContent();
-            String price = productPrice.getTextContent();
+                articles.add(new Article(productContent.getTextContent(), productPrice.getTextContent()));
+            }
 
-            System.out.printf("Product: %s\n Price: %s", product, price);
+            for (Article a : articles) {
+                System.out.printf("Product: %s\n Price: %s\n", a.getProductName(), a.getPrice());
+            }
+
+            webClient.getRefreshHandler();
+
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-        return content;
     }
 
 
