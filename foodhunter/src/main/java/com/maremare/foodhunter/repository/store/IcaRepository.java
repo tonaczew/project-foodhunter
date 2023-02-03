b4f203e9484352f862b94dbb213129efa4cbc4fa
Philip Tonaczew <tonaczew@gmail.com>
2023-02-03 17:21:23 +0100

    small try catch change

--- foodhunter/src/main/java/com/maremare/foodhunter/repository/store/IcaRepository.java
+++ foodhunter/src/main/java/com/maremare/foodhunter/repository/store/IcaRepository.java
@@ -1,8 +1,8 @@
 package com.maremare.foodhunter.repository.store;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
-import com.maremare.foodhunter.Article;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
@@ -22,19 +22,20 @@ public class IcaRepository {
             webClient.getOptions().setCssEnabled(false);
 
             for (String product : shoppingList) {
-                final HtmlPage page = webClient.getPage(url);
-                final HtmlForm searchForm = (HtmlForm) page.getByXPath("//form").get(0);
-                final HtmlInput inputTextField = searchForm.getInputByName("q");
-                final HtmlButton searchButton = (HtmlButton) searchForm.getByXPath("//button").get(0);
-                inputTextField.type(product);
-                HtmlPage resultPge = searchButton.click();
-                try{
+                try {
+                    final HtmlPage page = webClient.getPage(url);
+                    final HtmlForm searchForm = (HtmlForm) page.getByXPath("//form").get(0);
+                    final HtmlInput inputTextField = searchForm.getInputByName("q");
+                    final HtmlButton searchButton = (HtmlButton) searchForm.getByXPath("//button").get(0);
+                    inputTextField.type(product);
+                    HtmlPage resultPge = searchButton.click();
+
                     HtmlAnchor productContent = (HtmlAnchor) resultPge.getByXPath("//a[starts-with(@data-test,'fop-product-link')]").get(0);
                     HtmlStrong productPrice = (HtmlStrong) resultPge.getByXPath("//strong[starts-with(@data-test,'fop-price')]").get(0);
-                    responseData.put(productContent.getTextContent(),productPrice.getTextContent());
+                    responseData.put(productContent.getTextContent(), productPrice.getTextContent());
 
-                } catch(Exception IndexOutOfBoundsException){
-                    System.out.println("EXEPTION IndexOutOfBoundsException");
+                } catch (IndexOutOfBoundsException | FailingHttpStatusCodeException e) {
+                    System.out.println("EXEPTION");
                     responseData.put("Ingen sökträff: " + product, "-");
                 }
 
